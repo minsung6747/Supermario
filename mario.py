447276c0c5dc71499d04546b8d69e4824deaaf5d
minsung6747 <90056087+minsung6747@users.noreply.github.com>
2021-11-05 22:24:18 +0900

    11/05 마리오 맵 구현
    맵을 올바르게 구현한지는 잘모르겠음.

---
+++ mario.py
@@ -0,0 +1,23 @@
+from pico2d import *
+
+open_canvas(2561,217)
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+             running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+             running = False
+
+Map=load_image('map.png')
+running=True
+while running:
+    handle_events()
+    clear_canvas()
+    Map.draw(2561,217)
+    update_canvas()
+
+close_canvas()
+
